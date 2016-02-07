cd0cd1eded6f8c5565fe5c80d7f39614e9e98cdc
iguitarguy <atsou@ggc.edu>
2016-02-07 12:17:14 -0500

    enabled persistant posts/upvotes

--- MeanTutorial/flapper-news/public/javascripts/angularApp.js
+++ MeanTutorial/flapper-news/public/javascripts/angularApp.js
@@ -8,7 +8,12 @@ function($stateProvider, $urlRouterProvider) {
         .state('home', {
             url: '/home',
             templateUrl: '/home.html',
-            controller: 'MainCtrl'
+            controller: 'MainCtrl',
+            resolve: {
+                postPromise: ['posts', function(posts){
+                    return posts.getAll();
+                }]
+            }
         })
 
         .state('posts', {
@@ -19,7 +24,29 @@ function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('home');
 }])
+.factory('posts', ['$http', function($http){
+    var o = {
+        posts: []
+    };
+    o.getAll = function() {
+        return $http.get('/posts').success(function(data){
+            angular.copy(data, o.posts);
+        });
+    };
 
+    o.create = function(post) {
+        return $http.post('/posts', post).success(function(data){
+            o.posts.push(data);
+        });
+    };
+    o.upvote = function(post) {
+        return $http.put('/posts/' + post._id + '/upvote')
+            .success(function(data){
+            post.upvotes += 1;
+        });
+    };
+    return o;
+}])
 .controller('MainCtrl', [
 '$scope',
 'posts',
@@ -27,25 +54,18 @@ function($scope, posts){
     $scope.test = 'Hello world!';
     $scope.posts = posts.posts;
 
-    $scope.addPost = function() {
-        if ($scope.title === '') {
-            return;
-        }
-        $scope.posts.push({
+    $scope.addPost = function(){
+        if(!$scope.title || $scope.title === '') { return; }
+        posts.create({
             title: $scope.title,
             link: $scope.link,
-            upvotes: 0,
-            comments: [
-              {author: 'Joe', body: 'Cool post!', upvotes: 0},
-              {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-            ]
         });
         $scope.title = '';
         $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
-        post.upvotes += 1;
-    }
+        posts.upvote(post);
+    };
 
 }])
 
@@ -70,9 +90,3 @@ function($scope, $stateParams, posts) {
     };
 }])
 
-.factory('posts', [function(){
-    var o = {
-        posts: []
-    };
-    return o;
-}])
