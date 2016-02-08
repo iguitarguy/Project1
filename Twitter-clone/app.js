673d3aa76614cc3c8d995f0146dbfa4b64ba02b5
Cody Cates <Cody Cates>
2016-02-08 09:52:03 -0500

    Updated repository with prior work

--- Twitter-clone/app.js
+++ Twitter-clone/app.js
@@ -1,77 +1,79 @@
-// dependencies
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
-var routes = require('./routes/index');
+var mongoose = require("mongoose");
+require("./models/Posts");
+require("./models/Comments");
+require("./models/Users");
+
+var passport = require("passport");
+require("./config/passport");
+
+var indexRoutes = require("./routes/index");
+var partialsRoutes = require("./routes/partials");
+var postsRoutes = require("./routes/posts");
+var commentsRoutes = require("./routes/comments");
+var authRoutes = require("./routes/auth");
+var usersRoutes = require("./routes/users");
 
 var app = express();
 
-// template engine setup
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+mongoose.connect("mongodb://localhost/news");
 
-// middle-ware
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(passport.initialize());
-app.use(passport.session());
 
-// serves static files with express
-app.use(express.static(path.join(__dirname, 'views')));
-app.use('/', routes);
+app.use("/", indexRoutes);
+app.use("/", partialsRoutes);
+app.use("/", postsRoutes);
+app.use("/", commentsRoutes);
+app.use("/", authRoutes);
+app.use("/", usersRoutes);
 
-// passport config
-var Account = require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
-
-// mongoose
-mongoose.connect('mongodb://localhost/passport_local_mongoose_express');
-
-//catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handlers - need to set up for handling html errors
+// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
--- Twitter-clone/routes/index.js
+++ Twitter-clone/routes/index.js
@@ -1,49 +1,15 @@
-var express = require('express');
-var passport = require('passport');
-var Account = require('../models/account');
-var router = express.Router();
+(function() {
+  "use strict";
+  
+  var express = require("express");
+  var router = express.Router();
 
-// Renders the HTML files using the ejs template enging
-exports.index = function(request, response) {
-    res.render('index', { title: 'ejs' });
-};
+  router.route("/")
+    .get(function(req, res, next) {
+      res.render("index", {
+        title: "Express"
+      });
+    });
 
-router.get('/', function (request, response) {
-    response.render('index', { user : request.user });
-});
-
-router.get('/register', function(request, response) {
-    response.render('register', { });
-});
-
-router.post('/register', function(request, response) {
-    Account.register(new Account({ username : request.body.username }), 
-        request.body.password, function(err, account) {
-            if(err) {
-                return response.render('register', {info: "That username already exists."});
-            }
-
-            passport.authenticate('local')(request, response, function() {
-                response.redirect('/dashboard');
-            });
-        });
-});
-
-router.get('/login', function(request, response) {
-    response.render('login', { user : request.user });
-});
-
-router.post('/login', passport.authenticate('local'), function(request, response) {
-    response.redirect('/dashboard');
-});
-
-router.get('/logout', function(request, response) {
-    request.logout();
-    response.redirect('/');
-});
-
-router.get('/dashboard', function(request, response) {
-    response.render('dashboard');
-});
-
-module.exports = router;
+  module.exports = router;
+})();
